17476dde3af683ca2c1e798d2323e9bff6d87a87
shreyasskasetty-tamu <shreyas30kasetty@gmail.com>
2024-07-18 20:36:42 -0500

    Understanding dependency injection of different types with Spring Framework doing all the management. @Autowired, @Service, @Controller usage

--- src/main/java/com/skasetty/sfg_di/controllers/PropertyInjectedController.java
+++ src/main/java/com/skasetty/sfg_di/controllers/PropertyInjectedController.java
@@ -1,8 +1,13 @@
 package com.skasetty.sfg_di.controllers;
 
 import com.skasetty.sfg_di.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PropertyInjectedController {
+
+    @Autowired
     public GreetingService greetingService;
     public String getGreeting(){
         return greetingService.sayGreeting();
